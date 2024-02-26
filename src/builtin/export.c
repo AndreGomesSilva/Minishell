2c3dcd61d3648587f65ac4dc083fab8ce788accc
Renan Afonso Batista <rbatista@resutec.com.br>
2024-02-26 00:10:33 +0000

    fix: corrigido corte correto no substr na situação de variaveis normais no export.

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -12,14 +12,14 @@
 
 #include "../../include/minishell.h"
 
-void	export_valid(int *i, int position_equal, char **cmd, t_control *control)
+void	export_valid(int i, int position_equal, char **cmd, t_control *control)
 {
 	if (position_equal != -1)
-		update_env(control, ft_substr(cmd[*i], 0, position_equal),
-			ft_substr(cmd[*i], position_equal + 1, ft_strlen(cmd[*i]
-					- position_equal)), FALSE);
+		update_env(control, ft_substr(cmd[i], 0, position_equal),
+			ft_substr(cmd[i], position_equal + 1, ft_strlen(cmd[i]
+					) - position_equal -1), FALSE);
 	else
-		update_env(control, ft_substr(cmd[*i], 0, position_equal),
+		update_env(control, ft_substr(cmd[i], 0, position_equal),
 			ft_strdup(""), TRUE);
 }
 
@@ -41,7 +41,7 @@ void	handle_export_builtin(t_control *control, char **cmd)
 		str = ft_substr(cmd[i], 0, position_equal);
 		str = swap_string(ft_strdup("$"), str);
 		if (is_variable(str) && position_equal != 0)
-			export_valid(&i, position_equal, cmd, control);
+			export_valid(i, position_equal, cmd, control);
 		else
 		{
 			printf("%s%s%s\n", "export: `", cmd[i],
