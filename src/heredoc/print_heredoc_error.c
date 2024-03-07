402ba43fb617c5146efecbdbe5784fbb30fa2431
AndreGomesSilva <silva_andregomes@outlook.com>
2024-03-07 01:25:21 -0300

    fix: remove_quotes of eof heredoc when use ctrl + D

--- src/heredoc/print_heredoc_error.c
+++ src/heredoc/print_heredoc_error.c
@@ -6,7 +6,7 @@
 /*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 00:51:38 by angomes-          #+#    #+#             */
-/*   Updated: 2024/03/06 01:11:38 by angomes-         ###   ########.fr       */
+/*   Updated: 2024/03/07 01:21:48 by angomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 void	print_error_heredoc(char *eof)
 {
+	char *new_eof;
+
+	new_eof = remove_quotes(eof);
 	ft_putstr_fd("heredoc: delimited by end-of-file (wanted `", 2);
-	ft_putstr_fd(eof, 2);
+	ft_putstr_fd(new_eof, 2);
 	ft_putstr_fd("')\n",2);
+	free(new_eof);
 }
